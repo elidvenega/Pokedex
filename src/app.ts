87a74b609a6bf1cfdff25cb6f4ec9cf98f52447e
elid venega <venegaelid@gmail.com>
2020-09-17 19:37:06 -0700

    updated and working on typescript

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,54 @@
 const container: HTMLElement | any = document.getElementById("app")
-const pokemons: number = 100
+const pokemons: number = 20;
 
 interface IPokemon {
   id: number;
   name: string;
   image: string;
   type: string;
+  height: number;
 }
 
 const fetchData = (): void => {
-    for (let i = 1; i <= pokemons; i++) {
-      getPokemon(i)
-    }
+  for (let i = 1; i <= pokemons; i++) {
+    getPokemon(i)
   }
-  
-  const getPokemon = async (id: number): Promise<void> => {
-    const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const pokemon: any = await data.json()
-    const pokemonType: string = pokemon.types
-      .map((poke: any) => poke.type.name)
-      .join(", ")
-  
-    const transformedPokemon = {
-      id: pokemon.id,
-      name: pokemon.name,
-      image: `${pokemon.sprites.front_default}`,
-      type: pokemonType,
-    }
+}
 
-    showPokemon(transformedPokemon);
-  };
-  
-  const showPokemon = (pokemon: IPokemon): void => {
-    let output: string = `
+const getPokemon = async (id: number): Promise<void> => {
+  const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const pokemon: any = await data.json()
+  const pokemonType: string = pokemon.types
+    .map((poke: any) => poke.type.name)
+    .join(", ")
+
+  const transformedPokemon = {
+    id: pokemon.id,
+    name: pokemon.name,
+    image: `${pokemon.sprites.front_default}`,
+    type: pokemonType,
+    height: pokemon.height,
+
+  }
+
+  showPokemon(transformedPokemon);
+};
+
+const showPokemon = (pokemon: IPokemon): void => {
+  const dec = 'decimeter tall';
+  let output: string = `
           <div class="card">
               <span class="card--id">#${pokemon.id}</span>
               <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
               <h1 class="card--name">${pokemon.name}</h1>
               <span class="card--details">${pokemon.type}</span>
-          </div>
+              <p class="card--text">${pokemon.height} ${dec}</p>
+            </div>
       `;
-    container.innerHTML += output;
-  };
-  
-  fetchData();
+  container.innerHTML += output;
+};
+
+fetchData();
 
 
 
